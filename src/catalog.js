c1607b83586ddd59b2d48301c6cc3ba31b70e260
ryha25 <ryuyah12@gmail.com>
2026-09-22 16:13:58 +0900

    Initial MONST BOX Phase 1

---
+++ src/catalog.js
@@ -0,0 +1,9 @@
+export const characters = [
+  { id: "lucifer-jushinka-kai", name: "ルシファー", form: "獣神化・改", attribute: "光", type: "反射" },
+  { id: "mana-jushinka", name: "マナ", form: "獣神化", attribute: "火", type: "反射" },
+  { id: "elle-jushinka", name: "エル", form: "獣神化", attribute: "光", type: "貫通" },
+  { id: "neo-jushinka", name: "ネオ", form: "獣神化", attribute: "水", type: "反射" },
+  { id: "yakumo-jushinka", name: "ヤクモ", form: "獣神化", attribute: "木", type: "貫通" },
+  { id: "anastasia-jushinka", name: "アナスタシア", form: "獣神化", attribute: "水", type: "貫通" },
+  { id: "arsene-jushinka", name: "アルセーヌ", form: "獣神化", attribute: "闇", type: "反射" }
+];
